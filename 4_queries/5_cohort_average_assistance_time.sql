0e37c0050f55b229addaf4e774c3bb2b8aafc2a9
Fabio Papa <fabtheman@gmail.com>
2019-10-14 21:28:42 -0700

    Add cohort average assistance time query

---
+++ 4_queries/5_cohort_average_assistance_time.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name, AVG(completed_at - started_at)
+       FROM cohorts
+       JOIN students
+            ON cohorts.id = students.cohort_id
+       JOIN assistance_requests
+            ON students.id = assistance_requests.student_id
+       GROUP BY cohorts.name
+       ORDER BY AVG(completed_at - started_at);
