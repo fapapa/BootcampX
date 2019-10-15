b1f6fac6f9700de91fef0a3efd87979f0733d83b
Fabio Papa <fabtheman@gmail.com>
2019-10-14 20:35:40 -0700

    Add teacher assistance request count query

---
+++ 4_queries/1_teacher_assistance_requests_count.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(assistance_requests.*) AS total_assistances, name
+       FROM assistance_requests
+       JOIN teachers
+            ON teacher_id = teachers.id
+       WHERE name = 'Waylon Boehm'
+       GROUP BY name;
